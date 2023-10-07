21e121be4b0130b4c6b7607845baafb1539d4a41
Oqil Karimov <karimov.oqil@gmail.com>
2023-10-07 14:28:53 +0500

    fix settings

--- config/settings.py
+++ config/settings.py
@@ -11,13 +11,13 @@ ROOT_URLCONF = "config.urls"
 
 # Application definition
 INSTALLED_APPS = [
+    "apps.account",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "apps.account",
 ]
 
 MIDDLEWARE = [
